2ef0f7f6ea3923bfdc17899fe5cf4105c182394e
aka-achea <34272495+aka-achea@users.noreply.github.com>
2018-12-17 23:48:59 +0800

    a

--- arg.py
+++ arg.py
@@ -6,14 +6,15 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description = 'arg test')
     parser.add_argument('-s',help='Single song download')
-    parser.add_argument('-c','--cd',help='CD download')
+    parser.add_argument('-c','--cd',help='CD download',action='store_true')
     args = parser.parse_args()
 
     if args.s:
         print(args.s)
 
-    if args.cd:
-        print(args.cd)
+    if args.cd == True:
+        print('go')
+
 
 if __name__ == "__main__":
     main()
